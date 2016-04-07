d7e49771068a35d0e1ed64de6051087433eb31bd
Vincent Cheng <vscheng@outlook.com>
2016-04-07 07:12:38 -0400

    Template CMakeLists.txt

---
+++ trailingZeros/include/main.h
@@ -0,0 +1,16 @@
+//
+//  main.h
+//  trailingZeros
+//
+//  Created by AgentP on 2016-04-05.
+//  Copyright © 2016 Vincent Cheng. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+void greetings (void);
+
+#endif /* main_h */
---
+++ trailingZeros/src/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  trailingZeros
+//
+//  Created by AgentP on 2016-04-05.
+//  Copyright © 2016 Vincent Cheng. All rights reserved.
+//
+
+#include "main.h"
+
+#include <stdio.h>
+
+int main (void)
+{
+  greetings ();
+}
+
+void greetings (void)
+{
+  printf ("hello world\n");
+}
